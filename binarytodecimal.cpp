3e85357378ad5411e298338e82c8e07e8a64bb82
ArpitBalodi <arpitbalodi2@gmail.com>
2021-08-02 18:06:40 +0530

    newfiles

---
+++ binarytodecimal.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+    using namespace std;
+    int binaryToDecimal(int n){
+        int ans = 0;
+        int x = 1;
+        while (n>0)
+        {
+            int lastdigit = n % 10;
+            ans += x * lastdigit;
+            x*=2;
+            n/=10;
+        }
+        return ans;
+        
+    }
+ int main()
+{
+    int n;
+    cin>>n;
+    cout<<binaryToDecimal(n)<<endl;
+    return 0;
+}
